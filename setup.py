19b352df42034acb770c6b8fbc649c711489f594
William McCorkindale <wjm41@cam.ac.uk>
2022-02-03 14:54:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,32 @@
 from distutils.core import setup
 
 setup(
-    name='molplotly',         # How you named your package folder (MyLib)
-    packages=['molplotly'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
+    name='molplotly',
+    packages=['molplotly'],
+    version='0.1',
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    license='MIT',
-    # Give a short description about your library
+    license='Apache License, Version 2.0',
+
     description='TYPE YOUR DESCRIPTION HERE',
-    author='William McCorkindale',                   # Type in your name
-    author_email='wjm41@cam.ac.uk',      # Type in your E-Mail
-    # Provide either the link to your github or to your website
-    url='https://github.com/user/wjm41',
-    # I explain this later on
+    author='William McCorkindale',
+    author_email='wjm41@cam.ac.uk',
+
+    url='https://gitlab.com/wjm41/molplotly',
     download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
-    # Keywords that define your package best
+
     keywords=['science', 'chemistry', 'cheminformatics'],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'validators',
         'beautifulsoup4',
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        
         'Development Status :: 4 - Beta',
-        # Define that your audience are developers
+        
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Chemistry',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: Apache Software License'
+       
         'Programming Language :: Python :: 3',
     ],
 )
